2c9c23d59f6d91d5dafe235d8ca638c780d63011
Pedro Barbosa <jpedro.barbosa@gmail.com>
2016-08-19 04:17:45 +0200

    Seize front-end infrastructures

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/russross/blackfriday"
+	_ "github.com/lib/pq"
 )
 
 var (
 	repeat int
+	db *sql.DB
 )
 
 func repeatHandler(c *gin.Context) {
@@ -22,6 +24,39 @@ func repeatHandler(c *gin.Context) {
 	}
 	c.String(http.StatusOK, buffer.String())
 }
+
+func dbFunc(c *gin.Context) {
+    if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+        c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error creating database table: %q", err))
+        return
+    }
+
+    if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+        c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error incrementing tick: %q", err))
+        return
+    }
+
+    rows, err := db.Query("SELECT tick FROM ticks")
+    if err != nil {
+        c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error reading ticks: %q", err))
+        return
+    }
+
+    defer rows.Close()
+    for rows.Next() {
+        var tick time.Time
+        if err := rows.Scan(&tick); err != nil {
+          c.String(http.StatusInternalServerError,
+            fmt.Sprintf("Error scanning ticks: %q", err))
+            return
+        }
+        c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+    }
+}
+
 
 func main() {
 	var err error
@@ -67,6 +102,7 @@ func main() {
 	})
 
 	router.GET("/yo", repeatHandler)
+	router.GET("/db", dbFunc)
 
 	router.Run(":" + port)
 }
