11d182ed407fadf012f4bffed21413e8c4aec0aa
Pedro Barbosa <jpedro.barbosa@gmail.com>
2016-08-19 04:08:08 +0200

    Enhance open-source e-services

--- main.go
+++ main.go
@@ -9,12 +9,33 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+var (
+	repeat int
+)
+
+func repeatHandler(c *gin.Context) {
+    var buffer bytes.Buffer
+    for i := 0; i < repeat; i++ {
+        buffer.WriteString("Hello from Go!\n")
+    }
+    c.String(http.StatusOK, buffer.String())
+}
+
+
+
 func main() {
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+
+    tStr := os.Getenv("REPEAT")
+    repeat, err = strconv.Atoi(tStr)
+    if err != nil {
+        log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
+        repeat = 5
+    }
 
 	router := gin.New()
 	router.Use(gin.Logger())
@@ -43,6 +64,8 @@ func main() {
 	router.GET("/md", func(c *gin.Context) {
 		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
 	})
+
+	router.GET("/yo", repeatHandler)
 
 	router.Run(":" + port)
 }
