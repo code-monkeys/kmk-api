e3beabff7786f3d3103de71359b7150f491d9999
Pedro Barbosa <jpedro.barbosa@gmail.com>
2016-08-19 04:12:29 +0200

    Engage value-added solutions

--- main.go
+++ main.go
@@ -1,73 +1,57 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
-	"strconv"
-	"bytes"
+    "bytes"
+    "log"
+    "net/http"
+    "os"
+    "strconv"
 
-	"github.com/gin-gonic/gin"
-	"github.com/russross/blackfriday"
+    "github.com/gin-gonic/gin"
+    "github.com/russross/blackfriday"
 )
 
 var (
-	repeat int
+    repeat int
 )
 
 func repeatHandler(c *gin.Context) {
-	var buffer bytes.Buffer
-	for i := 0; i < repeat; i++ {
-		buffer.WriteString("Hello from Go!\n")
-	}
-	c.String(http.StatusOK, buffer.String())
+    var buffer bytes.Buffer
+    for i := 0; i < repeat; i++ {
+        buffer.WriteString("Hello from Go!\n")
+    }
+    c.String(http.StatusOK, buffer.String())
 }
 
-
-
 func main() {
-	port := os.Getenv("PORT")
+    var err error
+    port := os.Getenv("PORT")
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
-	}
+    if port == "" {
+        log.Fatal("$PORT must be set")
+    }
 
-	tStr := os.Getenv("REPEAT")
-	repeat, err = strconv.Atoi(tStr)
-	if err != nil {
-		log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
-		repeat = 5
-	}
+    tStr := os.Getenv("REPEAT")
+    repeat, err = strconv.Atoi(tStr)
+    if err != nil {
+        log.Printf("Error converting $REPEAT to an int: %q - Using default\n", err)
+        repeat = 5
+    }
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+    router := gin.New()
+    router.Use(gin.Logger())
+    router.LoadHTMLGlob("templates/*.tmpl.html")
+    router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+    router.GET("/", func(c *gin.Context) {
+        c.HTML(http.StatusOK, "index.tmpl.html", nil)
+    })
 
-	router.GET("/info", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+    router.GET("/mark", func(c *gin.Context) {
+        c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+    })
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+    router.GET("/repeat", repeatHandler)
 
-	router.GET("/user/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(http.StatusOK, "Hello %s", name)
-	})
-
-	router.GET("/md", func(c *gin.Context) {
-		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
-	})
-
-	router.GET("/yo", repeatHandler)
-
-	router.Run(":" + port)
+    router.Run(":" + port)
 }
